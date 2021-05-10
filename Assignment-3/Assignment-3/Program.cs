6f594cef05f930f20212c92bda66723ebf148e10
Deep khurana <marverev@gmail.com>
2021-05-10 17:26:37 +0530

    Completed the assignment -- added the solution of problem in Solution.cs and using it in the Program.cs

---
+++ Assignment-3/Assignment-3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Solution.DeleteNth(new int[] { 20, 37, 20, 21 }, 1);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Assignment-3/Assignment-3/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assignment_3
+{
+    class Solution
+    {
+        public static int[] DeleteNth(int[] arr, int x)
+        {
+            Dictionary<int, int> itemCount = new Dictionary<int, int>();
+            return arr.Where((val, idx) => {
+                if (itemCount.ContainsKey(val))
+                {
+                    itemCount[val]++;
+                }
+                else
+                {
+                    itemCount.Add(val, 1);
+                }
+                return itemCount[val] <= x;
+            }).ToArray();
+        }
+    }
+}
