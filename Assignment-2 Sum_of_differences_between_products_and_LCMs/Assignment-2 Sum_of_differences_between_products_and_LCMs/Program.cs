3bbc287bf21b2a0e63b21d45015571b262345ef7
Deep khurana <marverev@gmail.com>
2021-05-10 14:42:12 +0530

    Initialize project and added Solution.cs

---
+++ Assignment-2 Sum_of_differences_between_products_and_LCMs/Assignment-2 Sum_of_differences_between_products_and_LCMs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_2_Sum_of_differences_between_products_and_LCMs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Assignment-2 Sum_of_differences_between_products_and_LCMs/Assignment-2 Sum_of_differences_between_products_and_LCMs/Solution.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2_Sum_of_differences_between_products_and_LCMs
+{
+    class Solution
+    {
+        
+    }
+}
