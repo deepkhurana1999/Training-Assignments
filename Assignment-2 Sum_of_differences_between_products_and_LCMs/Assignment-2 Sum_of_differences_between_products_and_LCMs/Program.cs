a07b862f39059e691547a761ab9c7bebdf0c41cd
Deep khurana <marverev@gmail.com>
2021-05-10 16:05:45 +0530

    Completed the assignment -- added the solution of problem in Solution.cs and using it in the Program.cs

--- Assignment-2 Sum_of_differences_between_products_and_LCMs/Assignment-2 Sum_of_differences_between_products_and_LCMs/Program.cs
+++ Assignment-2 Sum_of_differences_between_products_and_LCMs/Assignment-2 Sum_of_differences_between_products_and_LCMs/Program.cs
@@ -6,7 +6,8 @@ namespace Assignment_2_Sum_of_differences_between_products_and_LCMs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[][] arr = new int[][] { new int[] { 1, 1 }, new int[] { 0, 0 }, new int[] { 13, 91 } };
+            Console.WriteLine(Solution.SumDifferencesBetweenProductsAndLCMs(arr));
         }
     }
 }
--- Assignment-2 Sum_of_differences_between_products_and_LCMs/Assignment-2 Sum_of_differences_between_products_and_LCMs/Solution.cs
+++ Assignment-2 Sum_of_differences_between_products_and_LCMs/Assignment-2 Sum_of_differences_between_products_and_LCMs/Solution.cs
@@ -6,6 +6,37 @@ namespace Assignment_2_Sum_of_differences_between_products_and_LCMs
 {
     class Solution
     {
-        
+        static int GCD(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                num1 = num1 + num2;
+                num2 = num1 - num2;
+                num1 = num1 - num2;
+            }
+            int result = num2 % num1;
+
+            if (result != 0)
+                return GCD(result, num1);
+
+            return num1;
+        }
+
+        public static int SumDifferencesBetweenProductsAndLCMs(int[][] pairs)
+        {
+            int lcmTotal = 0;
+            int productTotal = 0;
+            for (int i = 0; i < pairs.Length; i++)
+            {
+                int gcd = 1;
+
+                if (pairs[i][0] != 0 && pairs[i][1] != 0)
+                    gcd = GCD(pairs[i][0], pairs[i][1]);
+
+                productTotal += (pairs[i][0] * pairs[i][1]);
+                lcmTotal = lcmTotal + (pairs[i][0] * pairs[i][1]) / gcd;
+            }
+            return productTotal - lcmTotal;
+        }
     }
 }
