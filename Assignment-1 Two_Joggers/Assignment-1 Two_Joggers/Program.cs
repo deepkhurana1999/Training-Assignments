c2174852a8d6f0a3d61cf931932118114c30aeec
Deep khurana <marverev@gmail.com>
2021-05-10 13:46:29 +0530

    Intialize repository for assignments --Initalize assignment-1

---
+++ Assignment-1 Two_Joggers/Assignment-1 Two_Joggers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_1_Two_Joggers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
